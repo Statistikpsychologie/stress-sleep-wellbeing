a812ed8997f64061373afa258f233b5bd8f684b5
Statistikpsychologie <koesters21@gmx.de>
2026-04-10 20:58:09 +0200

    Update Analyse.R

--- scripts/Analyse.R
+++ scripts/Analyse.R
@@ -1,93 +1,92 @@
-# ---- 1. Daten laden ----
+# ---- 1. load data and Packages ----
 
-data <- read.csv("data.csv")
+data <- read.csv("data/data.csv")
 
 #Packages
 library(ggplot2)
 library(performance)
 
-# ---- 2. Daten bereinigen ----
+# ---- 2. clean data ----
 
-# Fehlende Werte prüfen
 colSums(is.na(data))
 
-# ---- 3. Exploration ----
+# ---- 3. Data Exploration ----
 
-## Allgemeine Übersicht
+## General overview
 str(data)
 summary(data)
 
-## Ausreißer-Analyse
+## Outlier Analysis
 
 # Stress
 boxplot(data$stress)
 stressstats <- boxplot.stats(data$stress)
 stressstats$out
 
-# Schlafstunden
+# Sleep Hours
 boxplot(data$sleep_hours)
 sleepstats <- boxplot.stats(data$sleep_hours)
 sleepstats$out
 
-# Schlafqualität
+# Sleep Quality
 boxplot(data$sleep_quality)
 sleepqstats <- boxplot.stats(data$sleep_quality)
 sleepqstats$out
 
-# Wohlbefinden
+# Wellbeing
 boxplot(data$wellbeing)
 wellbeingstats <- boxplot.stats(data$wellbeing)
 wellbeingstats$out
 
-# ---- 4. Korrelationen ----
+# ---- 4. Correlations ----
 cor(data[, c("stress", "sleep_hours", "sleep_quality", "wellbeing")])
 
-# ---- 5. Visualisierung ----
-# Stress & Wohlbefinden
+# ---- 5. Visualization ----
+# Stress & Wellbeing
 ggplot(data, aes(x = stress, y = wellbeing)) +
   geom_point() +
   geom_smooth(method = "lm")
 
 ggsave("stress_wellbeing.png")
 
-# Schlafqualität & Wohlbefinden
+# Sleep Quality & Wellbeing
 ggplot(data, aes(x = sleep_quality, y = wellbeing)) +
   geom_point() +
   geom_smooth(method = "lm")
 
 ggsave("sleep_quality_wellbeing.png")
 
-# ---- 6. Modelle ----
-# Modell ohne Moderation
+# ---- 6. Regression Models ----
+# Model without interaction
 model <- lm(wellbeing ~ stress + sleep_hours + sleep_quality, data = data)
 summary(model)
 
-# Modell mit Gender und Stress als Moderation
+# Model with gender as moderator
 data$gender <- as.factor(data$gender)
 model_mod_gender <- lm(wellbeing ~ stress * gender + sleep_hours + sleep_quality, data = data)
 summary(model_mod_gender)
 
-# Modell mit Stress und Alter als Moderation
+# Model with age as moderator
 model_mod_age <- lm(wellbeing ~ stress * age + sleep_hours + sleep_quality, data = data)
 summary(model_mod_age)
 
 
 
 
-# ---- 7. Diagnostik ----
+# ---- 7. Model Diagnostics ----
 par(mfrow = c(2,2))
 plot(model)
 
 check_model(model)
 
-# 7.1 Visualisierung
+# 7.1 Visualization
 png("diagnostic_plots.png", width = 800, height = 800)
 par(mfrow = c(2,2))
 plot(model)
 dev.off()
 
-# ---- 8. Standardisierung ----
-# Standardisierung zur besseren Vergleichbarkeit der Effekte
+# ---- 8. Standardization ----
+# Standardization for better comparability of effects
 data$stress_z        <- scale(data$stress)
 data$sleep_hours_z   <- scale(data$sleep_hours)
 data$sleep_quality_z <- scale(data$sleep_quality)
